b5ff75f1a5507f7bbd72bcdc442bd682491e538f
professionaldev527 <professionaldev527@gmail.com>
2024-09-04 14:09:46 +0530

    update 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,10 @@ var isLoggedIn = false;
 
 function App() {
   return (
-    <div className="container">
-      {/*Ternary Operator*/}
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}
-      {/*AND Operator*/}
-      {currentTime > 12 && <h1>Why are you still working?</h1>}
+    <div className="container">{
+      isLoggedIn ? <h1>Hello</h1> : <Login />
+      // currentTime > 12 && <h1>Why are you still working?</h1>
+    }
     </div>
   );
 }
